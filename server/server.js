52e7317df9bcf0d0332879f0364ee0c2ad46e1f5
Kleber C Batista <klebercal@gmail.com>
2017-01-20 10:30:29 -0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+let {ObjectID} = require('mongodb')
 
 let {mongoose} = require('./db/mongoose')
 let {Todo} = require('./models/todo')
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err)
   })
 
+})
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send()
+    } else {
+      res.send({todo})
+    }
+  }, (err) => {
+    res.status(404).send()
+  })
 })
 
 app.listen(3000, () => {
