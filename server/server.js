7f4bdc6f98779887a1a83d82d4f16a3eb4828cca
Kleber C Batista <klebercal@gmail.com>
2017-01-23 16:50:52 -0200

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -97,7 +97,6 @@ app.patch('/todos/:id', (req, res) => {
   })
 })
 
-// POST /users
 app.post('/users', (req, res) => {
   let body = _.pick(req.body, ['email', 'password'])
   let user = new User(body)
@@ -111,7 +110,6 @@ app.post('/users', (req, res) => {
   })
 })
 
-// POST /users/login
 app.post('/users/login', (req, res) => {
   let body = _.pick(req.body, ['email', 'password'])
 
