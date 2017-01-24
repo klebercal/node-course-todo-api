ac3f6a859110ff97f4ded12ef5788aeaf52c1f1e
Kleber C Batista <klebercal@gmail.com>
2017-01-24 10:20:55 -0200

    Update config to use json file

--- server/config/index.js
+++ server/config/index.js
@@ -1,14 +1,12 @@
 let env = process.env.NODE_ENV || 'development'
-console.log('env *******', env)
 
 switch (env) {
   case 'development':
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost/TodoApp'
-    break
-
   case 'test':
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost/TodoAppTest'
+    let config = require('./config.json')
+
+    Object.keys(config[env]).forEach((key) => {
+      process.env[key] = config[env][key]
+    })
     break
 }
